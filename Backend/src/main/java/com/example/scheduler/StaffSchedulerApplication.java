7281bd327575d5116fe1c55698def6e6082574f5
Ishan <ishan.dahal72@gmail.com>
2024-12-04 20:14:15 -0600

    Added back-end configuration

---
+++ Backend/src/main/java/com/example/scheduler/StaffSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffSchedulerApplication.class, args);
+	}
+
+}
